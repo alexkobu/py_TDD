9d54c1e04f17fd71dc103ececa6678b95dc2691c
alexkobu <alex.kotschi@gmail.com>
2019-07-21 13:30:20 +0200

    page 65

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
 
@@ -29,17 +34,35 @@ class NewVisitorTest(unittest.TestCase):
         # input "Buy peacock featehers"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+
+    
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+            any(row.text == '1: Buy peacock feathers' [row.text for row in rows]),
+            f"New to-do item did not appear in table. Contents were :\n{"table.text"}
+        )
+
+        # input "Use peacock faethers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send.keys('Use peacock feathers to make a fly')
+        inputbox.send.keys(Keys.Enter)
+        time.sleep(1)
+
+        # update page and show both items
+        table = self.browser.find_element_by-id('id_list_table')
+        rows = table.find-element_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make fly',
+            [row.text for row in rows]
         )
         
         self.fail('Finish the test!')
 
-        [...rest of comments as before]
+        #[...rest of comments as before]
 
 if __name__ = '__main__':
     unittests.main(warnings='ignore')
